3eee2787b39c89fba4c094bee3e4623ea6fba042
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-24 06:49:40 -0500

    adding cors and everything to do with user authentication

--- config/app.js
+++ config/app.js
@@ -13,4 +13,14 @@ let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 // Express application
 let app = express();
+// Cors application
 let cors = require('cors')
+// create a user model instance
+let userModel = require('../model/User');
+let User = userModel.User;
+let session = require('express-session')
+let passport = require('passport')
+let passportLocal = require('passport-local')
+let flash = require('connect-flash')
+passport.use(User.createStrategy());
+let localStrategy = passportLocal.Strategy;
