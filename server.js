1e9e178f764d0aad14cecadc4a51e792d73d1b26
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-24 06:42:49 -0500

    creating port

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var app = require('./server/config/app');
 var debug = require('debug')('server');
 var http = require('http');
 
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
+
