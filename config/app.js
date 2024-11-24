0fbadf990eef75f674335ef95b005f81fd4f7ed7
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-24 06:46:49 -0500

    added first section of app.js (http, path and express)

---
+++ config/app.js
@@ -0,0 +1,10 @@
+// For creating HTTP errors
+let createError = require('http-errors');
+// For Express
+let express = require('express');
+ // For handling and transforming file paths
+let path = require('path');
+// For parsing cookies
+let cookieParser = require('cookie-parser');
+// For logging HTTP requests
+let logger = require('morgan');
