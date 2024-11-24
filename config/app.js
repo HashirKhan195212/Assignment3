2d861735f7ebbd857919855b1f0c8d69afb236d9
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-24 06:47:28 -0500

    added routes (will use the routes later)

--- config/app.js
+++ config/app.js
@@ -8,3 +8,6 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 // For logging HTTP requests
 let logger = require('morgan');
+// Making routes
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
