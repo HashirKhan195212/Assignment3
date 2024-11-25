d568cc337cd23e1b90e612446c1193c82bb21571
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-25 00:11:29 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,7 +13,7 @@ router.get('Exam/index', function(req, res, next) {
 });
 router.get('/login', function(req,res,next){
     if(!req.user){
-      res.render('auth/login',{
+      res.render('Auth/login',{
         title:'Login',
         message:req.flash('loginmessage'),
         displayName:req.user ? req.user.displayName:'' 
